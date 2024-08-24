bb80a33cd34f2bc82a700604a7c8546574480437
Varduhi Vardanyan <vardvardanyan09@gmail.com>
2024-08-24 18:10:12 +0400

    home component is already ready

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,12 +1,27 @@
 import { Link } from "react-router-dom";
 import { constants } from "../../assets/constants";
 import styles from "./home.module.scss";
+import "../../assets/fonts/fonts.scss";
 
 function Home() {
   return (
     <div className={styles.homeWrapper}>
-      <Link to={constants.paths.signIn}>Sign In</Link>
-      <Link to={constants.paths.signUp}>Sign Up</Link>
+      <div className={styles.homeContent}>
+        <p>
+          If you are not registered on the site you must be{" "}
+          <span>
+            <Link to={constants.paths.signIn} className={styles.signBtn}>
+              SignIn
+            </Link>
+          </span>
+          , otherwise{" "}
+          <span>
+            <Link to={constants.paths.signUp} className={styles.signBtn}>
+              SignUp
+            </Link>
+          </span>
+        </p>
+      </div>
     </div>
   );
 }
